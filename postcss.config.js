3ff77a1b3ee8b27c777969112aac0e132bca794e
= <tiaanduplessis@hotmail.com>
2020-11-03 22:46:42 +0200

    Add autoprefixer and remove postcss-preset-env

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   plugins: [
-    require("postcss-preset-env")({ stage: 3 }),
+    // require("postcss-preset-env")({ stage: 4 }),
+    require('autoprefixer'),
     require("cssnano")({
       preset: "default",
     }),
